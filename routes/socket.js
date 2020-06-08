3577748f33c9f3c333be796f223a010cf41852b3
bobby <devils6889@gmail.com>
2020-06-08 00:14:29 -0400

    redis

--- routes/socket.js
+++ routes/socket.js
@@ -7,8 +7,7 @@ var jsonfile = require('jsonfile');
 var id, datas = {};
 
 var redis = require('redis');
-//var client = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true });
-var client = redis.createClient();
+var client = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true });
 
 client.on('error', function (err) {
   console.log('Redis error: ' + err);
