ffb903e50bdf3172e4579580c2594bf2177a2b45
bobby <devils6889@gmail.com>
2020-06-09 00:17:32 -0400

    remove redis, add mongo

--- routes/socket.js
+++ routes/socket.js
@@ -2,123 +2,157 @@
  * Serve content over a socket
  */
 
-var _ = require('lodash');
-var jsonfile = require('jsonfile');
-var id, datas = {};
+var _ = require("lodash");
+var jsonfile = require("jsonfile");
+var id,
+  datas = {};
 
-var redis = require('redis');
-var client = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true });
-
-client.on('error', function (err) {
-  console.log('Redis error: ' + err);
-});
-const EPISODES_PLAYED_KEY = 'episodes-played'
+const MongoClient = require("mongodb").MongoClient;
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const dbName = process.env.MONGO_DB || "mydb";
 
 module.exports = function (io) {
   return function (socket) {
-    socket.on('game:start', function (data) {
-      console.log('game:start ' + data.data.id);
+    socket.on("game:start", function (data) {
+      console.log("game:start " + data.data.id);
       id = data.data.id;
       datas[id] = data;
-      data.game.round = 'J';
-      data.buzzed_player = 'temp';
-      io.emit('round:start', data);
+      data.game.round = "J";
+      data.buzzed_player = "temp";
+      io.emit("round:start", data);
     });
 
-    socket.on('final_wager:submit', function (data) {
-      console.log('final_wager:submit ' + data.id + data.wager.name, data.wager.wager_amount);
+    socket.on("final_wager:submit", function (data) {
+      console.log(
+        "final_wager:submit " + data.id + data.wager.name,
+        data.wager.wager_amount
+      );
       datas[id].game.final_wagers.push(data.wager);
-      io.emit('final_wager:submit:done', data.wager)
+      io.emit("final_wager:submit:done", data.wager);
     });
 
-    socket.on('final:submit', function (data) {
-      console.log('final:submit ' + data.id + data.answer);
+    socket.on("final:submit", function (data) {
+      console.log("final:submit " + data.id + data.answer);
       datas[id].game.final_answers.push(data.answer);
-      io.emit('final:submit:done', data.answer)
+      io.emit("final:submit:done", data.answer);
     });
 
-    socket.on('buzz:attempt', function (data) {
-      id = data.id
+    socket.on("buzz:attempt", function (data) {
+      id = data.id;
       if (datas[id].game.buzzed_player == null) {
-        datas[id].game.buzzed_player = data.name
-        io.emit('buzz:success', data.name);
+        datas[id].game.buzzed_player = data.name;
+        io.emit("buzz:success", data.name);
       }
     });
 
-    socket.on('buzz:lock', function (id) {
-      console.log('got lock')
-      var placeholderName = '             '
-      datas[id].game.buzzed_player == placeholderName
-      io.emit('buzz:success', placeholderName);
+    socket.on("buzz:lock", function (id) {
+      console.log("got lock");
+      var placeholderName = "             ";
+      datas[id].game.buzzed_player == placeholderName;
+      io.emit("buzz:success", placeholderName);
     });
 
-    socket.on('stump:send', function () {
+    socket.on("stump:send", function () {});
 
+    socket.on("buzz:reset", function (parent_id) {
+      datas[parent_id].game.buzzed_player = null;
+      io.emit("buzz:reset:success");
     });
 
-    socket.on('buzz:reset', function (parent_id) {
-      datas[parent_id].game.buzzed_player = null
-      io.emit('buzz:reset:success')
-    });
-
-    socket.on('round:end', function (data) {
-      console.log('round:end ' + data.round);
-      if (data.round === 'J') {
-        data.round = 'DJ';
-        if (data.player_1.score < data.player_2.score && data.player_1.score < data.player_3.score) {
-          data.control_player = 'player_1';
+    socket.on("round:end", function (data) {
+      console.log("round:end " + data.round);
+      if (data.round === "J") {
+        data.round = "DJ";
+        if (
+          data.player_1.score < data.player_2.score &&
+          data.player_1.score < data.player_3.score
+        ) {
+          data.control_player = "player_1";
+        } else if (
+          data.player_2.score < data.player_1.score &&
+          data.player_2.score < data.player_3.score
+        ) {
+          data.control_player = "player_2";
+        } else if (
+          data.player_3.score < data.player_1.score &&
+          data.player_3.score < data.player_2.score
+        ) {
+          data.control_player = "player_3";
         }
-        else if (data.player_2.score < data.player_1.score && data.player_2.score < data.player_3.score) {
-          data.control_player = 'player_2';
-        }
-        else if (data.player_3.score < data.player_1.score && data.player_3.score < data.player_2.score) {
-          data.control_player = 'player_3';
-        }
-      }
-      else if (data.round === 'DJ') {
-        data.round = 'FJ';
+      } else if (data.round === "DJ") {
+        data.round = "FJ";
         data.control_player = undefined;
-        io.emit('final:start');
-      }
-      else if (data.round === 'FJ') {
-        data.round = 'end';
+        io.emit("final:start");
+      } else if (data.round === "FJ") {
+        data.round = "end";
 
-        var file = 'games/' + id + '-' + new Date().getTime() + '.json';
+        var file = "games/" + id + "-" + new Date().getTime() + ".json";
         jsonfile.writeFileSync(file, data, { spaces: 2 });
-        client.hset(EPISODES_PLAYED_KEY, id, true)
+
+        MongoClient.connect(url, function (err, db) {
+          if (err) {
+            console.log("Mongo error: " + err);
+          } else {
+            var dbo = db.db(dbName);
+            var players = [
+              data.player_1,
+              data.player_2,
+              data.player_3,
+              data.player_4,
+              data.player_5,
+              data.player_6,
+            ];
+            var playerNames = [];
+            players.forEach(function (player) {
+              if (player.name) {
+                playerNames.push(player.name);
+              }
+            });
+            var myobj = {
+              episode_id: id,
+              player_names: playerNames,
+              date: Date(),
+            };
+            dbo.collection("playedgames").insertOne(myobj, function (err, res) {
+              if (err) throw err;
+              console.log("1 document inserted");
+              db.close();
+            });
+          }
+        });
       }
       datas[id].game = data;
-      io.emit('round:start', datas[id]);
-    })
-
-    socket.on('board:init', function () {
-      console.log('board:init');
-      socket.emit('board:init', datas[id]);
+      io.emit("round:start", datas[id]);
     });
 
-    socket.on('game:init', function (data) {
-      console.log('game:init ' + data);
-      socket.emit('game:init', datas[data]);
+    socket.on("board:init", function () {
+      console.log("board:init");
+      socket.emit("board:init", datas[id]);
     });
 
-    socket.on('clue:start', function (data) {
-      console.log('clue:start ' + data);
-      socket.broadcast.emit('clue:start', data);
+    socket.on("game:init", function (data) {
+      console.log("game:init " + data);
+      socket.emit("game:init", datas[data]);
     });
 
-    socket.on('clue:daily', function (data) {
-      console.log('clue:daily');
-      socket.broadcast.emit('clue:daily', data);
+    socket.on("clue:start", function (data) {
+      console.log("clue:start " + data);
+      socket.broadcast.emit("clue:start", data);
     });
 
-    socket.on('clue:end', function (data) {
-      console.log('clue:end');
+    socket.on("clue:daily", function (data) {
+      console.log("clue:daily");
+      socket.broadcast.emit("clue:daily", data);
+    });
+
+    socket.on("clue:end", function (data) {
+      console.log("clue:end");
       datas[id].game = data;
-      socket.broadcast.emit('clue:end', data);
+      socket.broadcast.emit("clue:end", data);
     });
 
-    socket.on('game:reset', function (id) {
-      client.hdel(EPISODES_PLAYED_KEY, id);
-    })
+    socket.on("game:reset", function (id) {
+      // TODO - maybe reset mongo
+    });
   };
 };
