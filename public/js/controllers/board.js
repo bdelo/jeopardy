ffec1ca7f68b93c4964bfdb4a794421e6c7089bb
bdelo <devils6889@gmail.com>
2021-02-25 15:03:13 -0500

    add final jeop presentation

--- public/js/controllers/board.js
+++ public/js/controllers/board.js
@@ -1,99 +1,127 @@
-'use strict';
+"use strict";
 
-angular.module('myApp.controllers').
-  controller('BoardCtrl', function ($scope, $timeout, $modal, socket, currencyFilter) {
-    socket.emit('board:init');
+angular
+  .module("myApp.controllers")
+  .controller(
+    "BoardCtrl",
+    function ($scope, $timeout, $modal, socket, currencyFilter) {
+      socket.emit("board:init");
 
-    socket.on('board:init', function (data) {
-      console.log('board:init ' + !!data);
-      if (data) {
-        $scope.data = data.data;
-        $scope.game = data.game;
-        $scope.scoreHtml = buildScores();
-      }
-    })
-
-    function buildPlayerScore(player) {
-      var playerScore = ""
-      if (player && player.name) {
-        playerScore = '<div class="col-md-4 text-center player-total">' +
-          '<div class="player-name">' + player.name +
-          '</div><div class="player-score">' +
-          currencyFilter((player && player.score) || 0, '$', 0) +
-          '</div>' +
-          '</div>'
-      }
-      return playerScore
-    }
-
-    function buildScores() {
-      var output = '<div class="row">'
-      output = output.concat(buildPlayerScore($scope.game.player_1))
-      output = output.concat(buildPlayerScore($scope.game.player_2))
-      output = output.concat(buildPlayerScore($scope.game.player_3))
-      output = output.concat(buildPlayerScore($scope.game.player_4))
-      output = output.concat(buildPlayerScore($scope.game.player_5))
-      output = output.concat(buildPlayerScore($scope.game.player_6))
-      output = output.concat('</div>')
-      return output
-    }
-
-    socket.on('round:start', function (data) {
-      console.log('round:start');
-      if (modalInstance) {
-        modalInstance.close();
-      }
-
-      $scope.data = data.data;
-      $scope.game = data.game;
-
-      if (data.game.round === 'DJ') {
-        openModal();
-        $timeout(modalInstance.close, 5000);
-      }
-      else if (data.game.round === 'FJ') {
-        $scope.scoreHtml = buildScores();
-      }
-      else if (data.game.round === 'end') {
-        openModal();
-      }
-    });
-
-    var modalInstance;
-    function openModal(id) {
-      if (modalInstance) {
-        modalInstance.close();
-      }
-
-      modalInstance = $modal.open({
-        templateUrl: 'partials/boardclue',
-        controller: 'BoardClueCtrl',
-        backdrop: 'static',
-        size: 'lg',
-        openedClass: 'board-modal-open',
-        resolve: {
-          response: function () {
-            return {
-              id: id,
-              clue: $scope.data[id],
-              game: $scope.game,
-              scoreHtml: buildScores()
-            };
-          }
+      socket.on("board:init", function (data) {
+        console.log("board:init " + !!data);
+        if (data) {
+          $scope.data = data.data;
+          $scope.game = data.game;
+          $scope.scoreHtml = buildScores();
         }
       });
-    };
 
-    socket.on('clue:start', function (data) {
-      console.log('clue:start ' + data);
-      openModal(data);
-    });
-
-    socket.on('clue:end', function (data) {
-      console.log('clue:end');
-      $scope.game = data;
-      if (modalInstance) {
-        modalInstance.close();
+      function buildPlayerScore(player) {
+        var playerScore = "";
+        if (player && player.name) {
+          playerScore =
+            '<div class="col-md-4 text-center player-total">' +
+            '<div class="player-name">' +
+            player.name +
+            '</div><div class="player-score">' +
+            currencyFilter((player && player.score) || 0, "$", 0) +
+            "</div>" +
+            "</div>";
+        }
+        return playerScore;
       }
-    });
-  });
+
+      function buildScores() {
+        var output = '<div class="row">';
+        output = output.concat(buildPlayerScore($scope.game.player_1));
+        output = output.concat(buildPlayerScore($scope.game.player_2));
+        output = output.concat(buildPlayerScore($scope.game.player_3));
+        output = output.concat(buildPlayerScore($scope.game.player_4));
+        output = output.concat(buildPlayerScore($scope.game.player_5));
+        output = output.concat(buildPlayerScore($scope.game.player_6));
+        output = output.concat("</div>");
+        return output;
+      }
+
+      socket.on("round:start", function (data) {
+        console.log("round:start");
+        if (modalInstance) {
+          modalInstance.close();
+        }
+
+        $scope.data = data.data;
+        $scope.game = data.game;
+
+        if (data.game.round === "DJ") {
+          openModal();
+          $timeout(modalInstance.close, 5000);
+        } else if (data.game.round === "FJ") {
+          $scope.scoreHtml = buildScores();
+        } else if (data.game.round === "end") {
+          openModal();
+        }
+      });
+
+      var modalInstance;
+      function openModal(id) {
+        if (modalInstance) {
+          modalInstance.close();
+        }
+
+        modalInstance = $modal.open({
+          templateUrl: "partials/boardclue",
+          controller: "BoardClueCtrl",
+          backdrop: "static",
+          size: "lg",
+          openedClass: "board-modal-open",
+          resolve: {
+            response: function () {
+              return {
+                id: id,
+                clue: $scope.data[id],
+                game: $scope.game,
+                scoreHtml: buildScores(),
+              };
+            },
+          },
+        });
+      }
+
+      socket.on("clue:start", function (data) {
+        console.log("clue:start " + data);
+        openModal(data);
+      });
+
+      socket.on("clue:end", function (data) {
+        console.log("clue:end");
+        $scope.game = data;
+        if (modalInstance) {
+          modalInstance.close();
+        }
+      });
+
+      socket.on("fj:reveal", function (playerId, playerInfo) {
+        console.log("board got fj:reveal" + playerId + playerInfo);
+        if (modalInstance) {
+          modalInstance.close();
+        }
+        modalInstance = $modal.open({
+          templateUrl: "partials/finalreveal",
+          controller: "FinalRevealCtrl",
+          backdrop: "static",
+          size: "lg",
+          openedClass: "board-modal-open",
+          resolve: {
+            response: function () {
+              return {
+                playerId: playerId,
+                game: $scope.game,
+                scoreHtml: buildScores(),
+                playerInfo: playerInfo,
+              };
+            },
+          },
+        });
+      });
+    }
+  );
