87d666e8c5c0e81c693a9c2f44e4f88c93fb4157
bdelo <devils6889@gmail.com>
2021-02-25 15:55:53 -0500

    design tweaks

--- public/js/controllers/finalreveal.js
+++ public/js/controllers/finalreveal.js
@@ -7,14 +7,23 @@ angular
     function ($scope, $modalInstance, response, socket) {
       angular.extend($scope, response);
 
-      $scope.wagerRevealed = null;
-      $scope.finalAmount = null;
+      $scope.wagerRevealed = "";
+      $scope.finalAmount = formatMoney($scope.playerInfo.score);
 
       socket.on("fj:reveal-wager", function (playerId, finalScore) {
         if (playerId == $scope.playerId) {
-          $scope.wagerAmount = "$"+$scope.playerInfo.fj_wager;
-          $scope.finalAmount = "$"+finalScore;
+          $scope.wagerAmount = formatMoney($scope.playerInfo.fj_wager);
+          $scope.finalAmount = formatMoney(finalScore);
         }
       });
+
+      function formatMoney(number) {
+        return number.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        });
+      }
     }
   );
